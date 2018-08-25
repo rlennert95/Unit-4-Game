e5477c7eb4149e82bca7ff00286d0ce810bc82ca
GodZkotm54 <41457922+rlennert95@users.noreply.github.com>
2018-08-25 12:38:17 -0400

    add javascript/

--- javascript/game.js
+++ javascript/game.js
@@ -1,49 +1,63 @@
+var total = 0;
+var wins = 0;
+var losses = 0;
+var targetNum = [Math.floor(Math.random() * 120) + 20];
+console.log(total); 
+
+function initializeCalculator() {
+    total = 0;
+    targetNum =  [Math.floor(Math.random() * 120) + 20];
+
+    $("#score, #randomnumber").empty();
+    $("#score").text("Your total score is: " + total);
+    $("#randomnumber").text("Number to guess: " + targetNum );
+  }
+
+  function resultCheck () {             
+    if  (total === targetNum) { //not working
+        wins++;
+        $("#wins").text("Wins: " + wins);
+        alert("winner");
+        initializeCalculator()
+
+    } else if (total > targetNum) {
+        losses++;
+        $("#losses").text("Losses: " + losses);
+        alert("loser"); 
+        initializeCalculator()
+    }
+  }
+
+
 $(document).ready(function () {
 
-    var total = 0;
-    var wins = 0;
-    var losses = 0;
-    var targetNum = [Math.floor(Math.random() * 120) + 20]; //if I delete this string of code and make it 0, the if/else statements work but making it worth any other number does not work
-    console.log(targetNum);
-    console.log(total); 
-    
 
     $("#randomnumber").text("Number to guess: " + targetNum );
 
     $("#crystal1").on("click", function () {
         total += 10;
         $("#score").text("Your total score is: " + total);
+        resultCheck();
     });
 
     $("#crystal2").on("click", function () {
         total += 7;
         $("#score").text("Your total score is: " + total);
+        resultCheck();
     });
 
     $("#crystal3").on("click", function () {
         total += 9;
         $("#score").text("Your total score is: " + total);
+        resultCheck();
     });
 
     $("#crystal4").on("click", function () {
         total += 13;
         $("#score").text("Your total score is: " + total);
+        resultCheck();
     });
 
-//need function that checks if numbers equal
-
-    if  (total === targetNum) {
-        wins++;
-        $("#wins").text("Wins: " + wins);
-        alert("winner");
-
-    } else if (total >= targetNum) {
-        losses++;
-        $("#losses").text("Losses: " + losses);
-        alert("loser")
-    }
-
-     
     
 });
 
